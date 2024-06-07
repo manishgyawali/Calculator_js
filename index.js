36fb7371cd65789d1f9ceaf7257c2cc32778f393
manishgyawali <manishgyawali345@gmail.com>
2024-06-07 17:06:46 +0545

    js changed

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 let firstvalue = "";
 let secondvalue = "";
 let opt = "";
-let add = document.getElementById('display');
+let output = 0;
+let add = document.getElementById("display");
 
-Value =(num) =>{
+const Value = (num) => {
+  if (opt.length <= 0) {
+    firstvalue += num;
+    add.value = firstvalue;
+  } else {
+    secondvalue += num;
+    add.value += num;
+  }
+  console.log(firstvalue, secondvalue, opt);
+};
 
+result = () => {
+  if (opt == "+") 
+    {
+    console.log("+");
+  output=Number(firstvalue)+Number(secondvalue);
 
-}
+  } 
+  else if (opt == "-") {
+    console.log("-");
+output=Number(firstvalue)-Number(secondvalue);
 
+  }
+   else if (opt == "*") {
+    console.log("*");
+output=Number(firstvalue)*Number(secondvalue);
 
-result =() =>{
+  }
+   else if (opt == "/") {
+    console.log("/");
+output=Number(firstvalue)/Number(secondvalue);
 
-}
-operator =() =>{
-}
+  } 
+  else if (opt == "%") {
+    console.log("%");
+output=Number(firstvalue)%Number(secondvalue);
 
+  } 
+  else {
+    console.log("invalid operator");
+  }
+
+add.value=output;
+};
+
+const operator = (A) => {
+  opt = A;
+  add.value += opt;
+  
+};
